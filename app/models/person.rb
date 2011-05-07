c89c5916c65123d8309cac65def288a2d1357a56
Steven Garcia <stevendgarcia@gmail.com>
2011-05-07 10:52:44 +0200

    working now

--- app/models/person.rb
+++ app/models/person.rb
@@ -4,4 +4,7 @@ class Person < ActiveRecord::Base
   
   validates_presence_of :name
   
+  accepts_nested_attributes_for :phones, :allow_destroy => true, :reject_if => proc { |obj| obj.blank? }
+  
+  
 end
